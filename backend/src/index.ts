841c9e17216f5c6067e72bf2eb354ae31ee5862c
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-04-30 01:35:10 -0400

    Update prisma schema.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,43 +1,74 @@
 import { PrismaClient } from "@prisma/client";
-import express, { Request, Response } from "express"
+import express, { NextFunction, Request, Response } from "express"
 import http from 'http';
+import cors from 'cors';
+import session, { Session } from "express-session";
 import { Server } from 'socket.io';
+
+declare module "http" {
+    interface IncomingMessage {
+        session: Session & {
+            authenticated: boolean
+        }
+    }
+}
 
 const prisma = new PrismaClient();
 
 const app = express()
 const server = http.createServer(app);
 
+// const sessionMiddleware = session({
+//     secret: 'test',
+//     resave: false,
+//     saveUninitialized: false
+// })
+
+app.use(cors({ origin: '*' }));
+// app.use(sessionMiddleware);
 app.use(express.json());
 
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+});
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
-app.get('/polls/:poll', async (req: Request, res: Response) => {
+app.get('/polls/:id', async (req: Request, res: Response) => {
     const pollId = req.params.id;
+    console.log("Trying to get poll with id: ", req.params);
     const pollWithId = await prisma.poll.findUnique({
         where: {
             id: pollId
+        },
+        include: {
+            choices: true
         }
     });
 
-    res.json({ pollWithId });
+    res.json(pollWithId);
 });
 
-app.post('/polls/create', async (req: Request, res: Response) => {
+app.post('/polls/new', async (req: Request, res: Response) => {
 
     const reqBody = req.body;
     console.log("Post create called!", req.body)
 
+    const receivedChoices: string[] = req.body.pollChoices;
+    const newChoices = receivedChoices.map((val) => ({ title: val, votes: 0 }));
+
     const newPoll = await prisma.poll.create({
         data: {
-            name: reqBody.name,
-            timeLimit: reqBody.timeLimit,
+            name: reqBody.pollTitle,
+            timeLimit: reqBody.pollTimeLimit,
+            totalVotes: 0,
             choices: {
                 createMany: {
-                    data: reqBody.choices
+                    data: newChoices
                 }
             },
         },
@@ -46,8 +77,57 @@ app.post('/polls/create', async (req: Request, res: Response) => {
         }
     });
 
-    res.json({ newPoll })
+    res.json(newPoll)
 });
+
+// io.use((socket, next) => sessionMiddleware(socket.request as Request, {} as Response, next as NextFunction));
+
+
+io.on("connection", (socket) => {
+
+
+    // console.log("Someone connected! ", socket.id);
+    socket.on("JOIN_A_ROOM", (roomId) => {
+        console.log(`${socket.id} wants to join ${roomId}`);
+        socket.join(roomId);
+    });
+
+    socket.on("INCREASE", ({ roomId, pollId, choiceId }) => {
+        // socket.use((_, next) => {
+        //     console.log("Checking if user has already voted!")
+        // })
+        console.log("Trying to update poll with id: ", pollId);
+        prisma.poll.update({
+            where: {
+                id: pollId
+            },
+            data: {
+                totalVotes: {
+                    increment: 1
+                },
+                choices: {
+                    update: {
+                        where: {
+                            id: choiceId,
+                        },
+                        data: {
+                            votes: {
+                                increment: 1
+                            }
+                        }
+                    }
+                }
+            },
+            include: {
+                choices: true
+            }
+        }).then((val) => {
+            io.to(pollId).emit("updatePoll", val);
+
+        });
+
+    })
+})
 
 const port = 3001
 server.listen(port, () => {
