40a8b348c1d384183cc4901eb3608917e402adbf
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-05-01 15:28:08 -0400

    Update socket url.

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const socket = io('http://localhost:3001');
+const socket = io(process.env.NODE_ENV === 'development' ? "http://localhost:3001" : "https://poll-app-votes.herokuapp.com/");
 export default socket;
